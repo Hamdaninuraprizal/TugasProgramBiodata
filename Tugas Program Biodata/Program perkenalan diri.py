a2ec78252e7e811f81a4d56bcd97b909a0a93d3a
Hamdaninuraprizal <92880808+Hamdaninuraprizal@users.noreply.github.com>
2021-10-28 02:14:21 +0700

    push

---
+++ Tugas Program Biodata/Program perkenalan diri.py
@@ -0,0 +1,18 @@
+#Perkenalan diri
+print("Perkenalan Diri".center(70))
+
+fullname=str(input('Enter Your Full Name :\n'))
+idname=str(input('Enter Your Nickname :\n'))
+yourage=int(input('Enter Your Age :\n'))
+nonpm=int(input('Enter Your NPM :\n'))
+pbirth=str(input('Enter Your Place Of Birth :\n'))
+address=str(input('Enter Your Address :\n'))
+numberphone=int(input('Enter Your Phone Number :\n'))
+
+#Proses pengeluaran perkenalan diri
+
+print(input('\nPress [Enter] to display introduction\n'))
+print('Assalamualaikum,\nLet me introduce My self. My name is',fullname ,'but you can call me',idname+'.\nMy NPM is',nonpm ,'I was born in',pbirth ,'and i am',yourage ,'years old.')
+print('I am very  glad if you want to invite my house in',address,'.So, dont forget to call me before with the number',str(numberphone)+'.\nThank You.') 
+
+#Sekian perkenalan diri saya terima kasih
